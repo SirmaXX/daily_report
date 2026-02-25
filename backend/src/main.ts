75996d23754776ec39e0135dd66c3005c34b912d
SirmaXX <sea-97@hotmail.com>
2026-02-25 21:33:57 +0300

    update backend main ts cors setting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
